3258aa75e1a1f18a0ba9d4c0e17cf4d84f9b81a5
ozanuykun <ozanbote@gmail.com>
2021-06-18 12:01:21 +0300

    Extended 6.14.0.

--- package-shopping/SelectItemsTest.php
+++ package-shopping/SelectItemsTest.php
@@ -20,4 +20,21 @@ class SelectItemsTest extends \PHPUnit\Framework\TestCase
 
         Cart::delete('4432222345219');
     }
+
+    public function testSelectObjects()
+    {
+        $data = 
+        [
+            'product'       => 'Banana',
+            'price'         => '10',
+            'quantity'      => 3,
+            'serial-number' => '4432222345219'
+        ];
+
+        Cart::insert($data);
+
+        $this->assertEquals('Banana', Cart::selectObjects()[0]->product);
+
+        Cart::delete('4432222345219');
+    }
 }
