cf8e36aed9b95f87e678bb4d5498b4091be86440
ozanuykun <ozanbote@gmail.com>
2024-08-28 15:41:25 +0300

    Removed testNotTTFExtension test.

--- package-image/TTFTextTest.php
+++ package-image/TTFTextTest.php
@@ -39,18 +39,4 @@ class TTFTextTest extends Test\GDExtends
             $this->assertStringContainsString('invli.ttf', $e->getMessage());
         }
     }
-
-    public function testNotTTFExtension()
-    {   
-        try
-        {
-          GD::canvas(300, 300, 'white')
-            ->color('black')->fontSize(20)->load(self::dir . 'image.jpg')->x(250)->y(100)->angle(90)->ttftext('Ozan Uykun!')
-            ->generate('png', $generateFile = self::dir . 'tff-300-300.png');
-        }
-        catch( Exception\TTFExtensionException $e )
-        {
-            $this->assertStringContainsString('image.jpg.ttf', $e->getMessage());
-        }
-    }
 }
