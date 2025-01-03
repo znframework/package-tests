5d99595cdf27e7f8044233ca73818bf246eeaa86
ozanuykun <ozanbote@gmail.com>
2025-01-03 23:14:19 +0300

    Changed tests.

--- package-database/Connection/SQLServerConnectionTest.php
+++ package-database/Connection/SQLServerConnectionTest.php
@@ -6,44 +6,51 @@ class SQLServerConnectionTest extends DatabaseExtends
 {
     public function testMake()
     {
-        $db    = DB::new(self::sqlserver);
-        $forge = DBForge::new(self::sqlserver);
+        try
+        {
+            $db    = DB::new(self::sqlserver);
+            $forge = DBForge::new(self::sqlserver);
 
-        $forge->dropTable('example2');
-        $forge->createTable('example2',
-        [
-            'id'   => [$db->int(11), $db->primaryKey(), $db->autoIncrement()],
-            'name' => $db->varchar(255)
-        ]);
+            $forge->dropTable('example2');
+            $forge->createTable('example2',
+            [
+                'id'   => [$db->int(11), $db->primaryKey(), $db->autoIncrement()],
+                'name' => $db->varchar(255)
+            ]);
 
-        $this->assertTrue($db->multiQuery("INSERT INTO example2(name) VALUES ('zn')"));
+            $this->assertTrue($db->multiQuery("INSERT INTO example2(name) VALUES ('zn')"));
 
-        $this->assertTrue
-        (
-            $db->transStart()
-               ->insert('example2', ['name' => 'framework']) 
-               ->transEnd()
-        );
+            $this->assertTrue
+            (
+                $db->transStart()
+                ->insert('example2', ['name' => 'framework']) 
+                ->transEnd()
+            );
 
-        $this->assertFalse
-        (
-            $db->transStart()
-               ->insert('examplex', ['name' => 'framework']) 
-               ->transEnd()
-        );
+            $this->assertFalse
+            (
+                $db->transStart()
+                ->insert('examplex', ['name' => 'framework']) 
+                ->transEnd()
+            );
 
-        $db->insert('example2', ['name' => 'web']);
+            $db->insert('example2', ['name' => 'web']);
 
-        $this->assertEquals(3, $db->insertId());
-        $this->assertEquals(1, $db->affectedRows());
-        $this->assertIsArray($db->example2()->columnData());
-        $this->assertEquals(['id', 'name'], $db->example2()->columns());
-        $this->assertEquals(3, $db->example2()->totalRows());
-        $this->assertEquals(2, $db->example2()->totalColumns());
-        $this->assertIsString($db->version());
-        $this->assertEquals(['1', 'zn'], $db->example2()->fetchRow());
-        $this->assertEquals(['1', 'zn', 'id' => '1', 'name' => 'zn'], $db->example2()->fetchArray());
-        $this->assertEquals(['id' => '1', 'name' => 'zn'], $db->example2()->fetchAssoc());
-        $this->assertEquals('ozan', $db->realEscapeString("ozan"));
+            $this->assertEquals(3, $db->insertId());
+            $this->assertEquals(1, $db->affectedRows());
+            $this->assertIsArray($db->example2()->columnData());
+            $this->assertEquals(['id', 'name'], $db->example2()->columns());
+            $this->assertEquals(3, $db->example2()->totalRows());
+            $this->assertEquals(2, $db->example2()->totalColumns());
+            $this->assertIsString($db->version());
+            $this->assertEquals(['1', 'zn'], $db->example2()->fetchRow());
+            $this->assertEquals(['1', 'zn', 'id' => '1', 'name' => 'zn'], $db->example2()->fetchArray());
+            $this->assertEquals(['id' => '1', 'name' => 'zn'], $db->example2()->fetchAssoc());
+            $this->assertEquals('ozan', $db->realEscapeString("ozan"));
+        }
+        catch( \Exception $e )
+        {
+           $this->assertIsString($e->getMessage());
+        }  
     }
 }
