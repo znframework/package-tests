2a9a89d7ca8a908f04257ce70d3384e90b221a9f
ozanuykun <ozanbote@gmail.com>
2026-09-02 12:57:02 +0300

    Stabilize Restful GET tests

--- package-services/GetTest.php
+++ package-services/GetTest.php
@@ -5,24 +5,26 @@ use Restful;
 
 class GetTest extends \PHPUnit\Framework\TestCase
 {
+    private const JSON_URL = 'https://raw.githubusercontent.com/znframework/package-services/develop/composer.json';
+
     public function testGet()
     {
-        $this->assertIsObject(Restful::get('https://repo.packagist.org/p/znframework/znframework.json'));
+        $this->assertIsObject(Restful::get(self::JSON_URL));
     }
 
     public function testGetCallOptions()
     {
-        $this->assertIsObject(Restful::returntransfer(1)->get('https://repo.packagist.org/p/znframework/znframework.json'));
+        $this->assertIsObject(Restful::returntransfer(1)->get(self::JSON_URL));
     }
 
     public function testGetWithUrl()
     {
-        $this->assertIsObject(Restful::url('https://repo.packagist.org/p/znframework/znframework.json')->get());
+        $this->assertIsObject(Restful::url(self::JSON_URL)->get());
     }
 
     public function testGetWithSSLVerifyPeer()
     {
-        $this->assertIsObject(Restful::sslVerifypeer(false)->url('https://repo.packagist.org/p/znframework/znframework.json')->get());
+        $this->assertIsObject(Restful::sslVerifypeer(false)->url(self::JSON_URL)->get());
     }
 
     public function testGetXMLResponse()
